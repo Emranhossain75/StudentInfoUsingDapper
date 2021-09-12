9a79341869db4eef1b964d53a87ed6110a1f6d35
Emran <emran53hossain@gmail.com>
2021-09-12 16:53:18 +0600

    Procedure Part 2

---
+++ CrudDapper/DALStudent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using CrudDapper.Models;
+
+namespace CrudDapper
+{
+    public class DALStudent
+    {
+        public List<Student>GetStudent()
+        {
+            string cnnstring = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection cnn = new SqlConnection(cnnstring);
+            cnn.Open();
+            SqlCommand command = new SqlCommand("ViewStudent", cnn);
+            IDataReader reader = command.ExecuteReader(CommandBehavior.Default);
+            List<Student> IsData = new List<Student>();
+            while (reader.Read())
+            {
+                IsData.Add(new Student()
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    Age = Convert.ToInt32(reader["Age"]),
+                    Name = reader["Name"].ToString(),
+                    Gender = reader["Gender"].ToString(),
+                    Salary = reader["Salary"].ToString()
+
+                });
+                
+               
+            }
+            return IsData;
+        }
+    }
+}
---
+++ CrudDapper/Reports/ReportView.aspx.cs
@@ -0,0 +1,27 @@
+﻿using CrudDapper.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CrudDapper.Reports
+{
+    public partial class ReportView : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                List<Student> IsDatsa = new List<Student>();
+                DALStudent dALStudent = new DALStudent();
+                IsDatsa = dALStudent.GetStudent();
+                ReportViewer1.LocalReport.ReportPath = Server.MapPath("ListOfStudent.rdlc");
+                ReportViewer1.LocalReport.DataSources.Clear();
+                ReportViewer1.LocalReport.DataSources.Add
+                    (new Microsoft.Reporting.WebForms.ReportDataSource("DsData",IsDatsa));
+            }
+        }
+    }
+}
